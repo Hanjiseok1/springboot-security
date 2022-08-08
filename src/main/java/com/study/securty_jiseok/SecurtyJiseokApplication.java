3fcbc23e408035be8fe1ebc11fc9b25b9653e801
Hanjiseok1 <heemoon25@naver.com>
2022-08-08 19:13:41 +0900

    security study

---
+++ src/main/java/com/study/securty_jiseok/SecurtyJiseokApplication.java
@@ -0,0 +1,13 @@
+package com.study.securty_jiseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurtyJiseokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurtyJiseokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/securty_jiseok/SecurtyJiseokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.securty_jiseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurtyJiseokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
